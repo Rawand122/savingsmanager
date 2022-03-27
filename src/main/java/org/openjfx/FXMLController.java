8e8a764cbca9d319f4a3cc99c79912ec61b27353
Rawand122 <rawandhassan@hotmail.com>
2022-03-27 17:57:27 +0100

    adding icon to app

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -1,18 +1,10 @@
 package org.openjfx;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.URL;
@@ -21,81 +13,126 @@ import java.util.ResourceBundle;
 
 public class FXMLController implements Initializable {
 
+    public static final String CURRENT_INCOME = "Current Income: ";
+    public static final String CURRENT_EXPENSES = "Current Expenses: ";
+    public static final String CURRENT_SAVINGS = "Current Savings: ";
     Expenses expenses = Expenses.getInstance();
     Savings savings = Savings.getInstance();
     Income income = Income.getInstance();
-    double [] initialValues = getLastValues();
-
-    String currentScene = "home";
-
+    double[] initialValues = getLastValues();
 
 
     @FXML
-    Button overviewBut;
+    Label monthlyIncome;
     @FXML
-    AnchorPane anchorpane;
+    Label weeklyIncome;
     @FXML
-    Label incomeLabel;
+    Label yearlyIncome;
     @FXML
     Label welcomemessage;
     @FXML
-    Label currentExpenses,currentIncome,currentSavings;
+    Label currentExpenses;
     @FXML
-    Pane incomePane,savingsPane,expensesPane;
+    Label currentIncome;
     @FXML
-    TextField savingsText,incomeText,expensesText;
+    Label currentSavings;
+    @FXML
+    Pane incomePane;
+    @FXML
+    Pane savingsPane;
+    @FXML
+    Pane expensesPane;
+    @FXML
+    Pane homePane;
+    @FXML
+    TextField savingsText;
+    @FXML
+    TextField incomeText;
+    @FXML
+    TextField expensesText;
+    @FXML
+    TextField savingsText1;
+    @FXML
+    TextField incomeText1;
+    @FXML
+    TextField expensesText1;
 
-
-
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
     public FXMLController() throws IOException {
+
     }
 
 
-
-
-    public void switchToExpenses(ActionEvent event) throws IOException {
+    public void switchToExpenses() {
         incomePane.setVisible(false);
         savingsPane.setVisible(false);
+        homePane.setVisible(false);
         expensesPane.setVisible(true);
-        currentExpenses.setText("Current Expenses: "+ expenses.getExpenses());
+        currentExpenses.setText(CURRENT_EXPENSES + expenses.getExpenses());
     }
 
-    public void switchToSavings(ActionEvent event) throws IOException {
+    public void switchToSavings() {
         incomePane.setVisible(false);
+        homePane.setVisible(false);
+        expensesPane.setVisible(false);
         savingsPane.setVisible(true);
-        expensesPane.setVisible(false);
-        currentSavings.setText("Current Savings: "+ savings.getSavings());
+
+        currentSavings.setText(CURRENT_SAVINGS + savings.getSavings());
     }
 
 
-    public void switchToIncome(ActionEvent event) throws IOException {
+    public void switchToIncome() {
         savingsPane.setVisible(false);
         expensesPane.setVisible(false);
+        homePane.setVisible(false);
         incomePane.setVisible(true);
-        currentIncome.setText("Current Income: "+ income.getIncome());
+        currentIncome.setText(CURRENT_INCOME + income.getIncome());
     }
 
-    public void switchToHome(ActionEvent event) throws IOException {
+    public void switchToHome() {
         savingsPane.setVisible(false);
         expensesPane.setVisible(false);
         incomePane.setVisible(false);
+        homePane.setVisible(true);
     }
 
-    public void add(ActionEvent event) throws IOException {
+    public void add() throws IOException {
 
-        if(savingsPane.isVisible()){
+        if (savingsPane.isVisible()) {
             savings.addSavings(Double.parseDouble(savingsText.getText()));
-            currentSavings.setText("Current Savings: "+ savings.getSavings());
-        } else if (expensesPane.isVisible()){
+            savingsText.clear();
+            currentSavings.setText(CURRENT_SAVINGS + savings.getSavings());
+            saveData();
+        } else if (expensesPane.isVisible()) {
             expenses.addExpenses(Double.parseDouble(expensesText.getText()));
-            currentExpenses.setText("Current Expenses: "+ expenses.getExpenses());
-        } else if (incomePane.isVisible()){
+            expensesText.clear();
+            currentExpenses.setText(CURRENT_EXPENSES + expenses.getExpenses());
+            saveData();
+        } else if (incomePane.isVisible()) {
             income.addIncome(Double.parseDouble(incomeText.getText()));
-            currentIncome.setText("Current Income: "+ income.getIncome());
+            incomeText.clear();
+            currentIncome.setText(CURRENT_INCOME + income.getIncome());
+            saveData();
+        }
+    }
+
+    public void reduce() throws IOException {
+
+        if (savingsPane.isVisible()) {
+            savings.removeSavings(Double.parseDouble(savingsText1.getText()));
+            savingsText1.clear();
+            currentSavings.setText(CURRENT_SAVINGS + savings.getSavings());
+            saveData();
+        } else if (expensesPane.isVisible()) {
+            expenses.removeExpenses(Double.parseDouble(expensesText1.getText()));
+            expensesText1.clear();
+            currentExpenses.setText(CURRENT_EXPENSES + expenses.getExpenses());
+            saveData();
+        } else if (incomePane.isVisible()) {
+            income.removeIncome(Double.parseDouble(incomeText1.getText()));
+            incomeText1.clear();
+            currentIncome.setText(CURRENT_INCOME + income.getIncome());
+            saveData();
         }
     }
 
@@ -104,24 +141,28 @@ public class FXMLController implements Initializable {
         //read text file and get values / if file doesn't exist create
         File myFile = new File("datafile.txt");
         double[] values = new double[3];
-        if (myFile.createNewFile()){
-            BufferedWriter writer = new BufferedWriter(new FileWriter(myFile.getPath()));
-            writer.write("0.0,0.0,0.0");
+        if (myFile.createNewFile()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(myFile.getPath()))) {
 
-            writer.close();
-            values[0] = 0.0;
-            values[1] = 0.0;
-            values[2] = 0.0;
-        }else{
-            BufferedReader reader = new BufferedReader(new FileReader(myFile));
-            String line = reader.readLine().replaceAll("/[/]", "");
-            String[] line2 = line.split(",");
-            values = Arrays.stream(line2)
-                    .mapToDouble(Double::parseDouble)
-                    .toArray();
+                writer.write("0.0,0.0,0.0");
+                values[0] = 0.0;
+                values[1] = 0.0;
+                values[2] = 0.0;
+            }
+
+
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(myFile))) {
+                String line = reader.readLine().replaceAll("/[/]", "");
+                String[] line2 = line.split(",");
+                values = Arrays.stream(line2)
+                        .mapToDouble(Double::parseDouble)
+                        .toArray();
+
+            }
+
 
         }
-
         return values;
     }
 
@@ -135,13 +176,14 @@ public class FXMLController implements Initializable {
         values[1] = Savings.getInstance().getSavings();
         values[2] = Income.getInstance().getIncome();
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(myFile.getPath()));
-        String line = Arrays.toString(values);
-        String editedLine = line.replaceAll("\\[", "").replaceAll("\\]","");
-        writer.write(editedLine);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(myFile.getPath()))) {
+            String line = Arrays.toString(values);
+            String editedLine = line.replaceAll("\\[", "").replaceAll("\\]", "");
+            writer.write(editedLine);
 
 
-        writer.close();
+        }
+
 
     }
 
@@ -149,9 +191,16 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-            expenses.addExpenses(initialValues[0]);
-            savings.addSavings(initialValues[1]);
-            income.addIncome(initialValues[2]);
+        expenses.addExpenses(initialValues[0]);
+        savings.addSavings(initialValues[1]);
+        income.addIncome(initialValues[2]);
+
+        String incomeYear = String.format("%.2f", income.getIncome() * 12);
+        String weekIncome = String.format("%.2f", (income.getIncome() - expenses.getExpenses()) / 4);
+        String monthIncome = String.format("%.2f", income.getIncome() - expenses.getExpenses());
+        yearlyIncome.setText("Current yearly income: " + incomeYear);
+        monthlyIncome.setText("Current monthly disposal income: " + monthIncome);
+        weeklyIncome.setText("(per week: " + weekIncome + " )");
 
 
     }
--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -1,22 +1,11 @@
 package org.openjfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.io.*;
-import java.util.Arrays;
-import java.util.Objects;
 
 
 public class MainApp extends Application {
@@ -25,17 +14,17 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-  try{
+        try {
 
-      Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-      Scene scene = new Scene(root);
-      stage.setTitle("Money Management");
-      stage.setScene(scene);
-      stage.show();
+            Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("Money Management");
+            stage.setScene(scene);
+            stage.getIcons().add(new Image(String.valueOf(getClass().getResource("icon.png"))));
+            stage.show();
 
 
-
-  } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -44,8 +33,6 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-
 
 
 }
